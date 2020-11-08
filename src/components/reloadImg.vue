db37a1398145bf727f1a2cc7565235971cecbac3
fazaio <faza6028@gmail.com>
2020-11-08 10:41:24 +0700

    alhamdulillah tfjs predict from axios buffer done

--- src/components/reloadImg.vue
+++ src/components/reloadImg.vue
@@ -10,7 +10,9 @@
 
 <script>
 import axios from "axios";
-
+require("@tensorflow/tfjs-backend-cpu");
+require("@tensorflow/tfjs-backend-webgl");
+const cocoSsd = require("@tensorflow-models/coco-ssd");
 
 export default {
   props: ["src"],
@@ -25,25 +27,41 @@ export default {
   },
   methods: {
     play() {
-      this.interval = setInterval(() => {
-        this.reqImg();
-      }, 2000);
+      // this.interval = setInterval(() => {
+      //   this.tfjs();
+      // }, 10000);
+      this.tfjs()
     },
     pause() {
       clearInterval(this.interval);
     },
-    async reqImg() {
-      axios
-        .get(this.src, {
-          responseType: "arraybuffer",
-        })
-        .then((response) => {
-          this.imageSrc =
-            "data:image/jpeg;base64," +
-            new Buffer(response.data, "binary").toString("base64");
-        });
-    }
-  }
+    async createImg() {
+      return new Promise((resolve) => {
+        axios
+          .get(this.src, {
+            responseType: "arraybuffer",
+          })
+          .then((response) => {
+            console.log("response from axios");
+            let ima = "data:image/jpeg;base64," + new Buffer(response.data, "binary").toString("base64");
+            this.imageSrc = ima
+            const im = new Image();
+            im.crossOrigin = "anonymous";
+            im.src = ima;
+            im.onload = () => {
+              resolve(im);
+            };
+          });
+      });
+    },
+    async tfjs() {
+      console.log("Star Predict!");
+      const img = await this.createImg()
+      const model = await cocoSsd.load();
+      const predictions = await model.detect(img);
+      console.log(predictions);
+    },
+  },
 };
 </script>
 
